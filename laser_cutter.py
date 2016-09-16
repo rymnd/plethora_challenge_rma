8d73fdc64bfeeb8531003123753f982e1741ea74
rymnd <ray.r.ma@gmail.com>
2016-09-16 17:16:13 -0400

    updated overall structure for cutter and system components
    should be able to focus on geo components only now

--- laser_cutter.py
+++ laser_cutter.py
@@ -1,14 +1,19 @@
 #laser cutter - details regarding just the laser cutter system
 import laser_geo
+import math
 
 #generic laser cutter object
 class Cutter(object):
 	_v_max		#in/s
-	_kerf = 0	#offset kerf (in)
+	_kerf = 0	#offset kerf (in) (half of the padding)
 	_material_cost = 1	#material cost ($/in^2)
 	_machine_cost = 1	#machine time cost ($/s)
 
 	def __init__(self,v_max,kerf,material_cost,machine_cost):
+		self._v_max = v_max
+		self._kerf = kerf
+		self._material_cost = material_cost
+		self._machine_cost = machine_cost
 	
 	#calculate cost when given a geometry path (sum of material and machine cost)
 	def calculateCost(self,geoPath):
@@ -16,8 +21,19 @@ class Cutter(object):
 		
 	#calculate time that machine needs to go through path
 	def calculateTime(self,geoPath):
+		offset_geoPath = geoPath.offset(self._kerf)
+		ts = 0
+		for ele in offset_geoPath:
+			#check for type and calculate timing appropriately
+			if isinstance(ele,laser_geo.Edge):
+				ts += self._v_max * ele.L
+			elif isinstance(ele,laser_geo.Arc):
+				ts += self._v_max * math.exp(-1/ele.radius) * ele.L
+			else:
+				print "[ERR] Unexpected geometry type: "+repr(type(ele))
+		return ts
 	
 #particular cutter proposed in the Plethora coding challenge
 class PlethoraCutter(Cutter):
 	def __init__(self):
-		Cutter.__init__(self,0.5,0.1,0.75,0.07)
+		Cutter.__init__(self,0.5,0.1/2,0.75,0.07)
--- laser_system.py
+++ laser_system.py
@@ -1,10 +1,31 @@
 #laser cutter system - handles profile deserialization, user interaction
 import laser_geo
 import laser_cutter
+import json
 
+class Profile(object):
+	_jsonEdges = None
+	_jsonVertices = None
+	_vertices = None
+	_edges = None
+	geoPath = None
+	
+	def __init__(self,jsonText):
+		jsonObj = json.loads(jsonText)	#assume that the json file is valid
+		self._jsonEdges = jsonObj['Edges']
+		self._jsonVertices = jsonObj['Vertices']	#only needed for reference
+		
+		#order in path not important for stock challenge
+		for k,v in self._jsonEdges.iteritems():
+	
+	
 class PlethoraSystem(object):
 	_cutter = None
+	_profile = None
 	
 	def __init__(self,cutter):
+		self._cutter = cutter
 	
-	def processProfile(self,profile):
+	def processProfile(self,fileName):
+		f = load(fileName)
+		self._profile = Profile(f.read())
